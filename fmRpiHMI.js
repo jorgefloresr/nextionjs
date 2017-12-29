ce2b89f7e4a045b6920a79fa7636709f1ac3e058
Jorge Flores <Jorge.FloresR@latam.com>
2017-12-29 17:41:41 -0300

    reading fs files

--- fmRpiHMI.js
+++ fmRpiHMI.js
@@ -1,10 +1,6 @@
 var screen = require('./client');
 const { exec } = require('child_process');
 var fs = require('fs');
-var Promise = require("promise");
-// Wrap the io functions with ones that return promises.
-var readdir = Promise.convertNodeAsyncFunction(fs.readdir);
-var readfile = Promise.convertNodeAsyncFunction( fs.readFile );
 
 var buttonRPi = {
     id: "01",
@@ -38,33 +34,29 @@ screen.suscribeById(buttonAux.id, function () {
 function findMp3FilesInDir(path) {
     var tmpPlaylist = [];
     if (fs.lstatSync(path).isDirectory()) {
-        console.log(path+" is a directory");
-        var items = readdir(path);
-        console.log(items.length +" files inside");
+        console.log(path + " is a directory");
+        var items = fs.readdirSync(path);
+        console.log(items.length + " files inside");
         for (var i = 0; i < items.length; i++) {
             if (fs.lstatSync(path + items[i]).isDirectory()) {
                 tmpPlaylist = tmpPlaylist.concat(findMp3FilesInDir(path + items[i] + "/"));
-                console.log("playlist size: "+tmpPlaylist.length);
+                console.log("playlist size: " + tmpPlaylist.length);
             } else {
-                if(items[i].endsWith("mp3")){
-                    console.log("adding "+path+items[i]+" to the playlist");
-                    tmpPlaylist.push(path+items[i]);
+                if (items[i].endsWith("mp3")) {
+                    console.log("adding " + path + items[i] + " to the playlist");
+                    tmpPlaylist.push(path + items[i]);
                 }
             }
         }
-        callback(tmpPlaylist);
     }
-    console.log("returning playlist size: "+tmpPlaylist.length+" from "+path);
+    console.log("returning playlist size: " + tmpPlaylist.length + " from " + path);
     return tmpPlaylist;
 }
 
-function createPlaylist(path){
-    var playList = [];
-    findMp3FilesInDir(path, function(result){
-        playList = result;
-        console.log("final playlist size: "+playList.length);
-        console.log(playList.join("\n"));
-    });
+function createPlaylist(path) {
+    var playList = findMp3FilesInDir(path);
+    console.log("final playlist size: " + playList.length);
+    console.log(playList.join("\n"));
 }
 
 function updateButtonText(button) {
