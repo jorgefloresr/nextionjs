5f2ec7a9e8defe5eb65695a0259438e9d4e2d60e
Jorge Flores <Jorge.FloresR@latam.com>
2017-12-29 12:23:10 -0300

    testing execute system commands

--- fmRpiHMI.js
+++ fmRpiHMI.js
@@ -1,22 +1,51 @@
 var screen = require('./client');
 const { exec } = require('child_process');
 
+var buttonRPi = {
+    id: "01",
+    name: "b0",
+    text: "RPi"
+}
+
+var buttonAux = {
+    id: "03",
+    name: "b1",
+    text: "aux"
+}
+
 screen.connect();
-screen.suscribeById("01",function(){
-    console.log("boton 1 ok");
+screen.suscribeById(buttonRPi.id, function () {
+    console.log(buttonRPi.text + " pressed");
 });
 
-screen.suscribeById("03",function(){
-    console.log("boton 2 ok");
-});
-
-exec('./playFromMic.sh', (err, stdout, stderr) => {
-    if (err) {
-      // node couldn't execute the command
-      return;
+screen.suscribeById(buttonAux.id, function () {
+    if (buttonAux.text == "aux") {
+        buttonAux.text = "playing";
+        updateButtonText(buttonAux);
+        execSysCommand("./playFromMic.sh");
+    }else{
+        buttonAux.text = "aux";
+        updateButtonText(buttonAux);
+        execSysCommand("killall arecord");
     }
-  
-    // the *entire* stdout and stderr (buffered)
-    console.log(`stdout: ${stdout}`);
-    console.log(`stderr: ${stderr}`);
-  });
+});
+
+function updateButtonText(button){
+    screen.write.setText(button.name, button.text);
+}
+
+function execSysCommand(command, callback) {
+    exec(command, (err, stdout, stderr) => {
+        if (err) {
+            // node couldn't execute the command
+            return;
+        }
+
+        // the *entire* stdout and stderr (buffered)
+        if (callback) {
+            callback(stdout);
+        }
+        //console.log(`stdout: ${stdout}`);
+        //console.log(`stderr: ${stderr}`);
+    });
+}
