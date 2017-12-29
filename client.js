94fae5324a08350151949931cc15853790a4582c
Jorge Flores <Jorge.FloresR@latam.com>
2017-12-29 10:17:19 -0300

    first commit, based on the code of https://github.com/gregory-yet/Nextion

---
+++ client.js
@@ -0,0 +1,77 @@
+/*
+
+
+Nextion.js for Raspberry Pi / Node.js
+
+Grégory G.
+
+*/
+
+var SerialPort = require('serialport');
+
+var port = new SerialPort('/dev/ttyAMA0', {
+	baudRate: 9600
+});
+
+function init(){
+	port.on('open', function() {
+		console.log('Port ouvert sur /dev/ttyAMA0 @ 9600 bds');
+		write.setPage(0);
+	});
+
+	port.on('data', function(byte){
+		var data = byte.toString('hex').match(/.{1,2}/g);
+		readUart(data);
+	});
+}
+
+// write.uart("any command");
+// write.setPage(0);
+// write.setText("t0", "hey");
+// write.setVis("t0", true);
+// write.getPage(); return actual page on nextion (sendme command)
+
+var write = {
+	uart: function(cmd){
+		writeUart(cmd);
+	},
+	setPage: function(num){
+		writeUart('page '+num);
+	},
+	setText: function(cmp, txt){
+		writeUart(cmp+'.txt="'+txt+'"');
+	},
+	setVis: function(cmp, value){
+		var val = value ? "1":"0";
+		writeUart('vis '+cmp+','+val);
+	},
+	setColor: function(cmp, bco){
+		writeUart(cmp + ".bco=" + bco);
+		writeUart("ref " + cmp);
+	},
+	getPage: function(){
+		writeUart('sendme');
+	}
+};
+
+function writeUart(cmd){
+	port.write(hex(cmd));
+}
+
+function readUart(data){
+	console.log(data.join(" "));
+}
+
+function hex(str) {
+	var arr = [];
+	for (var i = 0, l = str.length; i < l; i ++) {
+		var ascii = str.charCodeAt(i);
+		arr.push(ascii);
+	}
+	arr.push(255);
+	arr.push(255);
+	arr.push(255);
+	return new Buffer(arr);
+}
+
+init();
